659d38b973ebfca354dffa3ba958ee0c461a6d79
team-69 <jwchoi0129@gmail.com>
2015-08-30 02:42:13 +0000

    Jquery Masonry 주입 / 게시글 보기 디자인 변경

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -4,20 +4,30 @@ class BoardController < ApplicationController
     
     def main
         
-        @post_all = Post.order("id desc").all
+        @post_first = Post.last
+        
+        @post_all = Post.where("id != #{@post_first.id}").order("id desc").all             
+       
         
     end
     
     def write
         
-        Post.create(user_id: current_user.id,
+        post = Post.create(user_id: current_user.id,
                     context: params[:context],
                     color: 0,
                     like_count: 0,
                     );
                     
         
-        render :text => ""
+        render :json => {
+                         context: post.context,
+                         color: post.color,
+                         like_count: post.like_count,
+                         reply_count: post.reply.size}
+        
+        # render :object => @post
+        
     end
     
     
