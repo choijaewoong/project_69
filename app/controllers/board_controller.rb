4e52434f1237c8710b936fc9c4dcf66e22bf41db
team-69 <jwchoi0129@gmail.com>
2015-09-06 14:12:29 +0000

    게시글 작성 시간 입력 수정 및 로고 삽입

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -3,59 +3,43 @@ class BoardController < ApplicationController
     before_action :authenticate_user!, only: [:main]
     
     def main
-        
         @post_first = Post.last 
         @post_all = Post.where("id != #{@post_first.id}").order("id desc").all            
     end
     
     def write
-        
-        post = Post.create(user_id: current_user.id,
+        p = Post.create(user_id: current_user.id,
                     context: params[:context],
                     color: 0,
                     like_count: 0)
-        
-        render :json => {
-                         p_id: post.id,
-                         context: post.context,
-                         color: post.color,
-                         like_count: post.like_count,
-                         reply_count: post.reply.size,
-                         time: post.created_at}
-        # render :json => { :post => post}
+        render :json => {:post => p,
+                         :reply_count => p.reply.count}
     end
     
     def detail
-        
         @selected_post = Post.find(params[:id])
         @reply_all = @selected_post.reply.order('id desc').all
-        
     end
     
     def reply
         
-        post = Reply.create(user_id: current_user.id,
+        r = Reply.create(user_id: current_user.id,
                              post_id: params[:id],
                              context: params[:context],
                              like_count: 0)
 
         render :json => {
-                         context: post.context,                         
-                         like_count: post.like_count,
-                         reply_count: Post.find(params[:id]).reply.count 
+                         :reply => r,
+                         :reply_count => Post.find(params[:id]).reply.count 
                          }
-                         
                          #user_id: post.id,                
-                         
     end
     
     def like        
-        
         user = current_user
         post = Post.find(params[:post_id]);       
         
         like_post = LikePost.where(:user_id => user , :post_id => post).take
-        
         if like_post.nil?
             
             LikePost.create( user_id: user.id,
@@ -67,9 +51,7 @@ class BoardController < ApplicationController
             post.like_count -= 1
             post.save
         end
-        
         render :text => post.like_count
     end
-    
 end
 
--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,2 +1,4 @@
 class Hashtag < ActiveRecord::Base
+    
+    # has_and_belongs_to_many
 end
