390ba0ec1141b95570fbd8623ffb7d5fe5c842d2
team-69 <jwchoi0129@gmail.com>
2015-09-19 09:13:12 +0000

    버튼 디자인 수정 / 페이스북 로그인 기능 추가

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,  :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   
   root 'board#main#색동_저고리'
   
