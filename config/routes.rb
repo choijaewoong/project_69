0de26ab7f8205aa13ccabe66e700844b3225a03e
team-69 <jwchoi0129@gmail.com>
2015-10-02 12:53:27 +0000

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users,  :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  
-  root 'board#main#색동_저고리'
-  
-  match ":controller(/:action(/:id))", :via => [:post, :get]
+    
+    root 'board#main#78'
+    
+    match ":controller(/:action(/:id))", :via => [:post, :get]
 
 end
